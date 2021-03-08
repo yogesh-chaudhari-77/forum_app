cff028f0f0b9049629316278f834f819dfd151ab
yogesh.chaudhari.77 <chaudhari.yogesh20@gmail.com>
2021-03-08 13:44:23 +1100

    Basic login and register screen layout ready

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from config.config import config
 
 app = Flask(__name__)
 
+# Defining a contex so that global variables can be accessed in the templates
+@app.context_processor
+def get_global_config():
+  return {"config": config}
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login", methods = ['GET'])
+def login_get():
+    return render_template("login.html")
+
+
+@app.route("/login", methods = ['POST'])
+def login_post():
+    
+    id = request.form['id']
+    password = request.form['password']
+
+    # call firebase and validate this details with stored data
+
+    if True :
+        res = { "status" : "success"}
+    else :
+        res = { "status" : "failed"}
+
+    return jsonify(res)
+
+
+@app.route("/register", methods = ['GET'])
+def register_get():
+    return render_template("register.html")
+
+
+@app.route("/register", methods = ['POST'])
+def register_post():
+    return "register_post"
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/ajaxFunction.js
@@ -0,0 +1,39 @@
+/*  
+    AJAX - Browser Support - Tutorialspoint
+    AJAX - Browser Support - Tutorialspoint (2021). Available at: https://www.tutorialspoint.com/ajax/ajax_browser_support.htm (Accessed: 3 March 2021).
+*/
+
+//Browser Support Code
+function ajaxFunction() {
+    var ajaxRequest;  // The variable that makes Ajax possible!
+    try {
+        // Opera 8.0+, Firefox, Safari 
+        ajaxRequest = new XMLHttpRequest();
+        return ajaxRequest;
+    } catch (e) {
+
+        // Internet Explorer Browsers
+        try {
+            ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            return ajaxRequest;
+        } catch (e) {
+
+            try {
+                ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                return ajaxRequest;
+            } catch (e) {
+
+                // Something went wrong
+                alert("Your browser broke!");
+                return false;
+            }
+        }
+    }
+}
+
+// Use this method for any server calls
+function send_request(ajaxRequest, method, url, data, additional_headers) {
+    ajaxRequest.open(method, url, true);
+    ajaxRequest.send(data);
+}
+
